057eba1dcdfa3c123f7a702aadd696643e581aee
dn.shin98@gmail.com <dn.shin98@gmail.com>
2020-05-29 20:42:08 +0900

    hekastk

--- newproject/settings.py
+++ newproject/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'likelion', 'static'),
 )
-STATIC_ROOT = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
